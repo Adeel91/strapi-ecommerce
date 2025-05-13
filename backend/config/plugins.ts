bcc0400d13f794d650cb829da275942d7c4fea85
Muhammad Adeel <muhammad_adeel91@yahoo.com>
2025-05-13 19:16:04 +0200

    feat: added jwt secret backend

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default ({ env }) => ({
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET', 'your-generated-jwt-secret-here'),
+      },
+    },
+  });
